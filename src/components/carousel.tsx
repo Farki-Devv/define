ce4457356077af465b3dc52b11426c23c4278863
Farrux <ganiboyevfarrux468@gmail.com>
2024-10-31 04:05:31 -0700

    define change carousel

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,52 +1,49 @@
+'use client'
 /* eslint-disable @next/next/no-img-element */
-// this file has a carousel component that returns a slide to us
 import {
 	Carousel,
 	CarouselContent,
 	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
 } from '@/components/ui/carousel'
+import Autoplay from 'embla-carousel-autoplay'
 
 export function CarouselPage() {
+	// Rasmlarni massivga saqlaymiz
+	const images = ['/my-slide.png', '/my-slide2.png', '/my-slide3.png']
+
 	return (
-		<>
-			<Carousel
-				opts={{
-					align: 'center',
-				}}
-				className='w-full flex gap-4 overflow-x-auto'
-			>
-				<CarouselContent className='flex'>
-					<CarouselItem className='flex gap-5 md:basis-1/2 lg:basis-1/2 py-14 mt-0.5 px-12'>
+		<Carousel
+			opts={{
+				align: 'center',
+				loop: true,
+			}}
+			plugins={[
+				Autoplay({
+					delay: 1000,
+				}),
+			]}
+			className='w-full flex gap-4 overflow-x-auto'
+		>
+			<CarouselContent className='flex'>
+				{images.map((src, index) => (
+					<CarouselItem
+						key={index}
+						className='flex gap-5 md:basis-1/3 lg:basis-1/3 py-14 mt-0.5 px-12'
+					>
 						<div className='rounded-2xl shadow-lg bg-white flex items-center justify-center w-[40rem] h-[22.5rem]'>
-							<div className=''>
-								<img
-									src={'/my-slide.png'}
-									alt=''
-									className='rounded-2xl object-contain'
-								/>
-							</div>
-						</div>
-						<div className='rounded-2xl shadow-lg bg-white flex items-center justify-center w-[40rem] h-[22.5rem]'>
-							<div className=''>
-								<img
-									src={'/my-slide2.png'}
-									alt=''
-									className='rounded-2xl object-contain'
-								/>
-							</div>
-						</div>
-						<div className='rounded-2xl shadow-lg bg-white flex items-center justify-center w-[40rem] h-[22.5rem]'>
-							<div className=''>
-								<img
-									src={'/my-slide3.png'}
-									alt=''
-									className='rounded-2xl object-contain'
-								/>
-							</div>
+							<img
+								src={src}
+								alt={`Slide ${index + 1}`}
+								className='rounded-2xl object-contain'
+							/>
 						</div>
 					</CarouselItem>
-				</CarouselContent>
-			</Carousel>
-		</>
+				))}
+			</CarouselContent>
+			<CarouselPrevious />
+			<CarouselNext />
+		</Carousel>
 	)
 }
